fb3a1e5e7052bbfd96fc9f64888e083bb7b40e60
Lokeshwaran R <lokesh11052007@gmail.com>
2025-03-29 01:17:58 +0530

    for the auth.controller

--- backend/src/routes/auth.route.js
+++ backend/src/routes/auth.route.js
@@ -1,5 +1,6 @@
 import express from "express";
-import { login, logout, signup } from "../controller/auth.controller.js";
+import { checkMath, login, logout, signup, updateProfile } from "../controller/auth.controller.js";
+import { protectRoute } from "../middleware/auth.middleware.js";
 
 const router = express.Router();
 
@@ -8,6 +9,10 @@ router.post("/signup", signup);
 router.post("/login", login);
 
 router.post("/logout", logout);
+
+router.put("/update-profile", protectRoute, updateProfile)
+
+router.get("/check", protectRoute, checkMath)
 
 export default router;
 
