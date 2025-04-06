50fd563bfca4a3d1d1bacf47bf046f56e74c751a
Lokeshwaran R <lokesh11052007@gmail.com>
2025-04-06 17:52:50 +0530

    updates with messages

--- backend/src/routes/message.route.js
+++ backend/src/routes/message.route.js
@@ -1,9 +1,12 @@
 import express from "express";
 import { protectRoutes } from "../middleware/auth.middleware.js";
-import { getUsersForSidebar } from "../controller/message.controller.js"
+import { getMessages, getUsersForSidebar, sendMessage } from "../controller/message.controller.js"
 
 const router = express.Router();
 
 router.get("/users", protectRoutes, getUsersForSidebar)
+router.get("/:id",  protectRoutes, getMessages)
 
+
+router.post("/send/:id", protectRoutes, sendMessage)
 export default router;
