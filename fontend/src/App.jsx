d7ba8cbcee31bec3b1cf199942845baf54810bdb
Lokeshwaran R <lokesh11052007@gmail.com>
2025-04-06 20:36:46 +0530

    Added components & pages

--- fontend/src/App.jsx
+++ fontend/src/App.jsx
@@ -1,6 +1,27 @@
+import Navbar from "./components/Navbar"
+ 
+import HomePage from "./pages/HomePage"
+import SignUpPage from "./pages/SignUpPage"
+import LoginPage from "./pages/LoginPage"
+import SettingPage from "./pages/SettingPage"
+import ProfilePage from "./pages/ProfilePage"
+
+import { Routes, Route} from "react-router-dom"
+
 const App = () => {
   return (
-    <div className="text-red-600">App</div>
+    <div>
+      <Navbar />
+
+      <Routes>
+        <Route path="/" element={<HomePage/>} />
+        <Route path="/signup" element={<SignUpPage/>} />
+        <Route path="/login" element={<LoginPage/>} />
+        <Route path="/settings" element={<SettingPage/>} />
+        <Route path="/profile" element={<ProfilePage/>} />
+
+      </Routes>
+    </div>
   )
 }
 
