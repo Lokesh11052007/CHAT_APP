07abeb223eafd0c60e5a3babefb924209fc1e6e9
Lokeshwaran R <lokesh11052007@gmail.com>
2025-04-12 03:03:48 +0530

    signup page and connected frontend to backend working perfectly

--- fontend/src/App.jsx
+++ fontend/src/App.jsx
@@ -11,6 +11,8 @@ import { useAuthStore } from "./store/useAuthStore"
 import { useEffect } from "react"
 
 import { Loader } from "lucide-react" 
+import { Toaster } from "react-hot-toast"
+
 const App = () => {
   const {authUser, checkAuth, isCheckingAuth} = useAuthStore();
 
@@ -20,7 +22,8 @@ const App = () => {
 
   console.log({authUser})
 
-  if(isCheckingAuth && !authUser) return (
+  if(isCheckingAuth && !authUser) 
+    return (
     <div  className="flex items-center justify-center h-screen">
       <Loader className="size-10 animate-spin" />
     </div>
@@ -36,8 +39,9 @@ const App = () => {
         <Route path="/login" element={!authUser ? <LoginPage/> : <Navigate to="/"/>} />
         <Route path="/settings" element={<SettingPage/>} />
         <Route path="/profile" element={authUser ? <ProfilePage/> : <Navigate to="/login"/> } />
-
       </Routes>
+
+      <Toaster />
     </div>
   )
 }
