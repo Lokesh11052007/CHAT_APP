e50c1ca3e598aa2d008efbe36219b094e7a38df7
Lokeshwaran R <lokesh11052007@gmail.com>
2025-04-17 23:32:11 +0530

    setting pages

--- fontend/src/pages/HomePage.jsx
+++ fontend/src/pages/HomePage.jsx
@@ -1,6 +1,12 @@
 const HomePage = () => {
   return (
-    <div>HomePage</div>
+    <div>
+      <button className="btn btn-primary m-4 relative top-12">Test Button</button>
+<div className="bg-base-100 text-base-content p-4 rounded-box shadow-xl">
+  This should be themed.
+</div>
+    </div>
+
   )
 }
 
