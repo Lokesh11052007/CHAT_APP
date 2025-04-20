39771c131b34b74d41661c4fec5d782bd0f275f9
Lokeshwaran R <lokesh11052007@gmail.com>
2025-04-21 02:07:26 +0530

    updated

--- fontend/src/pages/HomePage.jsx
+++ fontend/src/pages/HomePage.jsx
@@ -1,11 +1,22 @@
+import { useChatStore } from "../store/useChatStore"
+import Sidebar from "../components/Sidebar"
+import NoChatSelected from '../components/NoCharSelected'
+import ChatContainer from "../components/ChatContainer"
 const HomePage = () => {
+  const { selectedUser } = useChatStore();
+
   return (
-    <div>
-      <button className="btn btn-primary m-4 relative top-12">Test Button</button>
-<div className="bg-base-100 text-base-content p-4 rounded-box shadow-xl">
-  This should be themed.
-</div>
+   <div className="h-screen bg-base-200">
+    <div className="flex items-center justify-center pt-20 px-4">
+      <div className="bg-base-100 rounded-lg shadow-cl w-full max-w-6xl h-[calc(100vh-8rem)]">
+         <div className="flex h-full rounded-lg overflow-hidden">
+          <Sidebar />
+
+          {!selectedUser ? <NoChatSelected /> : <ChatContainer /> }
+         </div>
+      </div>      
     </div>
+   </div>
 
   )
 }
