6b156f772e31683076ff35b1ad2c1e6fcbdc4804
Lokeshwaran R <lokesh11052007@gmail.com>
2025-04-23 23:53:18 +0530

    started

--- fontend/src/components/ChatContainer.jsx
+++ fontend/src/components/ChatContainer.jsx
@@ -1,4 +1,13 @@
+import React, { useEffect } from 'react'
+import { useChatStore } from '../store/useChatStore'
+
 const chatContainer = () => {
+
+  const {messages, getMessages, selectedUser, isMessageLoading} = useChatStore();
+
+  useEffect(() => {
+    getMessages(selectedUser._id);
+  }, [selectedUser._id, getMessages]);
   return (
     <div>chatContainer</div>
   )
