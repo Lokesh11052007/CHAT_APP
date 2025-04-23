7d3b6398c1aeccc4be1095a2510e08b8f1e5e374
Lokeshwaran R <lokesh11052007@gmail.com>
2025-04-23 23:57:56 +0530

    going 2 create return requirements

--- fontend/src/components/ChatContainer.jsx
+++ fontend/src/components/ChatContainer.jsx
@@ -1,15 +1,23 @@
-import React, { useEffect } from 'react'
+import { useEffect } from 'react';
 import { useChatStore } from '../store/useChatStore'
 
 const chatContainer = () => {
 
   const {messages, getMessages, selectedUser, isMessageLoading} = useChatStore();
 
-  useEffect(() => {
-    getMessages(selectedUser._id);
-  }, [selectedUser._id, getMessages]);
+ useEffect(() => {
+  getMessages(selectedUser._id)
+ },[selectedUser._id,getMessages])
+
+  if(isMessageLoading) return <div>Loading...</div>
   return (
-    <div>chatContainer</div>
+    <div className='flex-1 flex flex-col overflow-auto'>
+      <ChatHeader />
+
+      <p>messages...</p>
+
+      <MessageInput />
+    </div>
   )
 }
 
