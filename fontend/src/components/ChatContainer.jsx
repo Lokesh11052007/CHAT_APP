44acc2a071e6a9c18059c1fae0bcc2af2de6d845
Lokeshwaran R <lokesh11052007@gmail.com>
2025-04-29 13:43:41 +0530

    time

--- fontend/src/components/ChatContainer.jsx
+++ fontend/src/components/ChatContainer.jsx
@@ -5,6 +5,8 @@ import MessageInput from "./MessageInput";
 import MessageSkeleton from "./skeletons/MessageSkeleton";
 
 import { useAuthStore } from "../store/useAuthStore";
+
+import { formatMessageTime } from "../lib/utils";
 const ChatContainer = () => {
   const {messages, getMessages, isMessageLoading, selectedUser }=useChatStore();
 
@@ -45,9 +47,10 @@ const ChatContainer = () => {
              </div>
              <div className="chat-header mb-1"> 
               <time className="text-xs opacity-50 ml-1">
-                {message.createdAt}</time>
+                {formatMessageTime(message.createdAt)}
+              </time>
              </div>
-             <div className="chat-bubble flex">
+             <div className="chat-bubble flex flex-col">
               {message.image && (
                 <img 
                     src={message.image}
