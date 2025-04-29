58db744e96dde0d3d218dc6d6e95c34c27934636
Lokeshwaran R <lokesh11052007@gmail.com>
2025-04-29 13:31:22 +0530

    updated messages

--- fontend/src/store/useChatStore.js
+++ fontend/src/store/useChatStore.js
@@ -26,7 +26,7 @@ export const useChatStore = create((set,get) => ({
         set({ isMessageLoading: true});
         try{
             const res = await axiosInstance.get(`/messages/${userId}`)
-            Set({ messages: res.data})
+            set({ messages: res.data})
         }catch (error){
             toast.error(error.response.data.message)
         }finally{
