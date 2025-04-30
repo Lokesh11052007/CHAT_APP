872fbf39bde09c9646898e59f1ea874d39a9d242
Lokeshwaran R <lokesh11052007@gmail.com>
2025-04-30 21:37:06 +0530

    imported socketio

--- backend/src/index.js
+++ backend/src/index.js
@@ -7,8 +7,10 @@ import { connectDB } from "./lib/db.js";
 import cookieParser from "cookie-parser"
 import cors from "cors"
 
+import { app, server } from "./lib/socket.js";
+
 dotenv.config()
-const app = express();
+
 
 const PORT = process.env.PORT
 
@@ -23,7 +25,7 @@ app.use(cors({
 app.use("/api/auth", authRoutes);
 app.use("/api/messages",messageRoutes)
 
-app.listen(PORT, () => {
+server.listen(PORT, () => {
     console.log("Server is running on PORT: "+ PORT);
     connectDB();
 });
