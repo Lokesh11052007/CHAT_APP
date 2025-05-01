e43636bbd1354f7007a28a99759015e3d5cb77bf
Lokeshwaran R <lokesh11052007@gmail.com>
2025-05-01 11:41:57 +0530

    live message using socketIO

--- fontend/src/components/ChatContainer.jsx
+++ fontend/src/components/ChatContainer.jsx
@@ -8,12 +8,17 @@ import { useAuthStore } from "../store/useAuthStore";
 
 import { formatMessageTime } from "../lib/utils";
 const ChatContainer = () => {
-  const {messages, getMessages, isMessageLoading, selectedUser }=useChatStore();
+  const {messages, getMessages, isMessageLoading, selectedUser, subscribeToMessages, unsubscribeFromMessages }=useChatStore();
 
   const { authUser } = useAuthStore();
   useEffect(() => {
     getMessages(selectedUser._id);
-  },[selectedUser._id, getMessages])
+
+    subscribeToMessages();
+    return () => {
+      unsubscribeFromMessages();
+    };
+  },[selectedUser._id, getMessages, subscribeToMessages, unsubscribeFromMessages]);
 
   if(isMessageLoading) {
     return (
